6946c3f01369f209b558fcb47705935b409e0eac
rrimm <119832853+rrimm@users.noreply.github.com>
2023-03-13 12:14:28 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route} from "react-router-dom";
 import Routes from './routes';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
